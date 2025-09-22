b05c5413e9d7309c1acd4c1595163f386d65d6f9
Thais Damasceno <tssdamasceno@gmail.com>
2025-09-22 08:37:27 -0700

    TM ios storage module

---
+++ ios/modules/NativePingStorage/NativePingStorage.h
@@ -0,0 +1,17 @@
+//
+//  NativePingStorage.h
+//  RNSampleApp
+//
+//  Created by Thais Damasceno on 2025-09-18.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativePingStorage/NativePingStorage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativePingStorage : NSObject <NativePingStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
